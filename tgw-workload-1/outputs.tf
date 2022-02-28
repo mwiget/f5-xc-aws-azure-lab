00a66d4f0a8ee5291329f7621b98d1de464b7560
Marcel Wiget <m.wiget@f5.com>
2022-02-28 05:27:14 +0000

    fix workloads, using separate jumphost and workload

--- tgw-workload-1/outputs.tf
+++ tgw-workload-1/outputs.tf
@@ -1,3 +1,9 @@
+output jumphost_private_ip {
+  value = aws_instance.f5-jumphost-1.private_ip
+}
+output jumphost_public_ip {
+  value = aws_instance.f5-jumphost-1.public_ip
+}
 output workload_private_ip {
   value = aws_instance.f5-workload-1.private_ip
 }
--- tgw-workload-2/outputs.tf
+++ tgw-workload-2/outputs.tf
@@ -1,3 +1,9 @@
+output jumphost_private_ip {
+  value = aws_instance.f5-jumphost-2.private_ip
+}
+output jumphost_public_ip {
+  value = aws_instance.f5-jumphost-2.public_ip
+}
 output workload_private_ip {
   value = aws_instance.f5-workload-2.private_ip
 }
