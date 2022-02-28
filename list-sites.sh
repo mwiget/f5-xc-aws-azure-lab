9c601a03dca91a34d4b9f018702bd3835386c117
Marcel Wiget <m.wiget@f5.com>
2022-02-28 05:55:12 +0000

    show only running instances and make will now list ip from sites and workloads

--- list-sites.sh
+++ list-sites.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-for site in marcel-aws1-tgw-site-1 marcel-aws2-tgw-site-2; do
+for site in marcel-aws-tgw-site-1 marcel-aws-tgw-site-2; do
   echo "$site:" 
   aws ec2 describe-instances \
-  --filters Name=tag:ves-io-creator-id,Values=m.wiget@f5.com \
-  --filters Name=tag:ves-io-site-name,Values=$site \
+  --filters \
+    Name=tag:ves-io-creator-id,Values=m.wiget@f5.com \
+    Name=tag:ves-io-site-name,Values=$site \
+    Name=instance-state-name,Values=running \
   --query "Reservations[*].Instances[*].[Tags[?Key=='Name'].Value|[0],PublicIpAddress,PrivateIpAddress]" \
   --output=text | sort
   echo ""
