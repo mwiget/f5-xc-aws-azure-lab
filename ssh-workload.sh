ec1e4ce1681a9eae47739c8fba1bbe55b6e3f1ff
Marcel Wiget <m.wiget@f5.com>
2022-03-07 14:13:38 +0000

    use jumphost

--- ssh-workload.sh
+++ ssh-workload.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 site=$1
-prefix="marcel-aws-f5-xc-jumphost"
+prefix="marcel-aws-f5-xc"
 if [ -z "$site" ]; then
   site=1
 fi
 shift
 
-name="$prefix-$site"
+name="$prefix-jumphost-$site"
 echo -n "ssh ubuntu@$name ... "
-ip=$(aws ec2 describe-instances \
+ipj=$(aws ec2 describe-instances \
   --filters Name=instance-state-name,Values=running \
   Name=tag:Name,Values=$name \
   --query "Reservations[*].Instances[*].PublicIpAddress" \
   --output=text)
+  echo -n "(via $ipj) ... "
+
+name="$prefix-workload-$site"
+ip=$(aws ec2 describe-instances \
+  --filters Name=instance-state-name,Values=running \
+  Name=tag:Name,Values=$name \
+  --query "Reservations[*].Instances[*].PrivateIpAddress" \
+  --output=text)
   echo "($ip) ... "
 
-ssh ubuntu@$ip $@
+ssh -J ubuntu@$ipj ubuntu@$ip $@
